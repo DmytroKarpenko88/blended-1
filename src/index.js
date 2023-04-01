78684aac0057fa837cd385162f908f375ead76a7
Dmytro Karpenko <kardmitriy@gmail.com>
2023-04-01 16:18:49 +0300

    Task1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/normalize.css';
 import './styles/index.css';
 import { fetchAllProducts, fetchProductById } from './requests/products';
-import { markupAllProducts, markupSingleProduct } from './services/markupService';
-
+import {
+  markupAllProducts,
+  markupSingleProduct,
+} from './services/markupService';
 
 const allProductsRef = document.querySelector('#allProducts');
 
@@ -20,20 +22,32 @@ async function renderAllProducts() {
 
 // renderAllProducts();
 
-
-
-
-
-
 const submitBtn = document.querySelector('#singleProductForm');
-const container = document.querySelector('#singleProduct')
+const container = document.querySelector('#singleProduct');
 
-submitBtn.addEventListener('submit', onFormSubmit)
+submitBtn.addEventListener('submit', onFormSubmit);
 
 async function onFormSubmit(e) {
-  e.preventDefault()
+  e.preventDefault();
   const value = e.target.elements.id.value;
   const { data } = await fetchProductById(value);
 
-  container.innerHTML = markupSingleProduct(data)
+  container.innerHTML = markupSingleProduct(data);
 }
+///
+//
+///
+///
+//
+
+// const submitBtn = document.querySelector('#singleProductForm');
+// const container = document.querySelector('#singleProduct')
+
+// submitBtn.addEventListener('submit', onFormSubmit)
+
+// async function onFormSubmit(e) {
+//   e.preventDefault()
+//   const value = e.target.elements.id.value;
+//   const { data } = await fetchProductById(value);
+
+//   container.innerHTML = markupSingleProduct(data)
